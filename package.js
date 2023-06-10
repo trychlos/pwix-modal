28a97a67aef9d81ce32b97fdedac6495091aae6b
Pierre Wieser <pwieser@trychlos.org>
2023-06-10 10:49:02 +0200

    Fix outdated pwix:jquery-ui dependency

--- package.js
+++ package.js
@@ -34,7 +34,7 @@ function configure( api ){
     api.use( 'ecmascript' );
     api.use( 'less@4.0.0', 'client' );
     api.use( 'pwix:i18n@1.0.0' );
-    api.use( 'pwix:jquery-ui@0.1.0' );
+    api.use( 'pwix:jquery-ui' );
     api.use( 'pwix:layout@1.2.0' );
     api.use( 'random', 'client' );
     api.use( 'tmeasday:check-npm-versions@1.0.2', 'server' );
