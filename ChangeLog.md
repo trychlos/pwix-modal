b1a998bf8d8648e7ecefed529398803753bd321f
Pierre Wieser <pwieser@trychlos.org>
2023-09-11 12:10:00 +0200

    Post-release minimal version bump

--- ChangeLog.md
+++ ChangeLog.md
@@ -2,6 +2,12 @@
 
 ## ChangeLog
 
+### 1.7.3-rc
+
+    Release date: 
+
+    - 
+
 ### 1.7.2
 
     Release date: 2023- 9-11
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:modal',
-    version: '1.7.2',
+    version: '1.7.3-rc',
     summary: 'A Bootstrap-based package which provides draggable and resizable modal dialogs to Meteor',
     git: 'https://github.com/trychlos/pwix-modal',
     documentation: 'README.md'
